9536e0704f9160f7f4c27819ff549dea04ec93d5
Sara Peco <148265244+sarapecoCS@users.noreply.github.com>
2025-06-01 14:35:45 +0200

    fix

---
+++ backend/Bioskopina.Services/Migrations/20250601122119_awards.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bioskopina.Services.Migrations
+{
+    public partial class awards : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.AlterColumn<string>(
+           name: "Awards",
+           table: "Bioskopina",
+           type: "nvarchar(max)",
+           nullable: true,  
+           oldClrType: typeof(string),
+           oldType: "nvarchar(max)");
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.AlterColumn<string>(
+           name: "Awards",
+           table: "Bioskopina",
+           type: "nvarchar(max)",
+           nullable: false,  // <-- revert to NOT nullable here
+           oldClrType: typeof(string),
+           oldType: "nvarchar(max)",
+           oldNullable: true);
+
+        }
+    }
+}
---
+++ backend/Bioskopina.Services/Migrations/20250601122748_directorfix.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bioskopina.Services.Migrations
+{
+    public partial class directorfix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.AlterColumn<string>(
+               name: "Director",
+               table: "Bioskopina",
+               type: "nvarchar(max)",
+               nullable: true,
+               oldClrType: typeof(string),
+               oldType: "nvarchar(max)");
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.AlterColumn<string>(
+               name: "Director",
+               table: "Bioskopina",
+               type: "nvarchar(max)",
+               nullable: false,
+               defaultValue: "",
+               oldClrType: typeof(string),
+               oldType: "nvarchar(max)",
+               oldNullable: true);
+
+        }
+    }
+}
