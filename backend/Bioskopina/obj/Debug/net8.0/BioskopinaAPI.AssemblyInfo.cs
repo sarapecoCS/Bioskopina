aceab3e780becd00ef5acb21b738303a77a0e77f
Sara Peco <148265244+sarapecoCS@users.noreply.github.com>
2025-07-02 12:39:01 +0200

    add

--- backend/Bioskopina/obj/Debug/net8.0/BioskopinaAPI.AssemblyInfo.cs
+++ backend/Bioskopina/obj/Debug/net8.0/BioskopinaAPI.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BioskopinaAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16d960b4894d8723d1185713f4684c4c72a3007a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4695a9e83183b6156745e62e61b0c57d4160aa4")]
 [assembly: System.Reflection.AssemblyProductAttribute("BioskopinaAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BioskopinaAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
